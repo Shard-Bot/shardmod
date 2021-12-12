1f83719c71b192ccd46e6a20f9705e6562b4d4dc
Fabio <4r3n4asf4b10@gmail.com>
2021-12-12 17:31:51 -0400

    Añadiendo Comando user y nuevas variables

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,4 +1,6 @@
 import { Command, Interaction, Structures } from 'detritus-client';
+import { DiscordAbortCodes, Permissions} from 'detritus-client/lib/constants';
+import { PermissionTools} from 'detritus-client/lib/utils';
 
 export function isSnowflake(value: string): boolean {
   if (![16, 17, 18].includes(value.length) || isNaN(parseInt(value))) {
@@ -13,21 +15,38 @@ export async function fetchUserById(
   userId: string,
 ) {
   if (!isSnowflake(userId)) return null;
-
+  const {guild} = context;
   if (context.user.id === userId) {
-    return context.user;
+    return context.member || context.user;
   }
 
   try {
+    if(guild){
+    if (guild.members.has(userId)) {
+      return guild.members.get(userId);
+    } else {
+      return await guild.fetchMember(userId)
+    }
+  }
     if (context.users.has(userId)) {
       return context.users.get(userId) as Structures.User;
     }
 
     return await context.rest.fetchUser(userId);
-  } catch {
-    return null
+  } catch(error:any) {
+    switch (error.code) {
+      case DiscordAbortCodes.UNKNOWN_MEMBER: {
+        return await context.rest.fetchUser(userId);
+      };
+      case DiscordAbortCodes.UNKNOWN_USER: {
+        return null;
+      };
+      default: {
+        throw error;
+      };
+    }
   }
-
+return null
 }
 
 export async function getUserByText(context: Command.Context, text: string) {
@@ -35,11 +54,11 @@ export async function getUserByText(context: Command.Context, text: string) {
   const { guild } = context;
 
   if (guild) {
-    const member = context.users.get(text) ||
+    const member = guild.members.get(text) ||
       guild.members.find(member =>
         (member.user.tag.toLowerCase() === text.toLowerCase()) ||
         (member.name.toLowerCase().includes(text.toLowerCase()))
-      )?.user
+      )
 
     return member ?? null
   }
@@ -47,11 +66,23 @@ export async function getUserByText(context: Command.Context, text: string) {
   return null
 }
 
-export function getUserByMention(context: Command.Context): Structures.User | null {
-  if (context.message.mentions) {
-    const mention = context.message.mentions.first()!
-    if (mention instanceof Structures.Member) return mention.user
-    return mention
+export function getMemberJoinPosition(
+  guild: Structures.Guild,
+  userId: string,
+): [number, number] {
+  let members;
+  members = guild.members.sort((x, y) => x.joinedAtUnix - y.joinedAtUnix);
+  const joinPosition = members.findIndex((m) => m.id === userId) + 1;
+  return [joinPosition, guild.members.length];
+}
+
+export function permissionsToObject(permissions: bigint | number): Record<string, boolean> {
+  const result: Record<string, boolean> = {};
+  for (let check of Object.values(Permissions)) {
+    if (check === Permissions.NONE) {
+      continue;
+    }
+    result[String(check)] = PermissionTools.checkPermissions(permissions, check);
   }
-  return null;
+  return result;
 }
