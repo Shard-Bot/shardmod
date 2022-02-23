80bb6d6e278d267059f75d18d50e4995642cec1f
this.fatand <4r3n4asf4b10@gmail.com>
2022-02-22 23:18:38 -0400

    Editando categoria owner

---
+++ src/commands/prefix/owner/commands.reset.ts
@@ -0,0 +1,36 @@
+import { Command, CommandClient } from 'detritus-client';
+import { BaseCommand } from '../basecommand';
+import config from '../../../../config.json';
+import glob from 'glob';
+export const COMMAND_NAME = 'reload cmd';
+export default class EvalCommand extends BaseCommand {
+	constructor(client: CommandClient) {
+		super(client, {
+			name: COMMAND_NAME,
+			aliases: ['reload commands'],
+			metadata: {
+				description: 'Recarga los comandos del bot',
+				type: 'owner',
+			},
+		});
+	}
+
+	onBeforeRun(context: Command.Context) {
+		return config.devsIds.includes(context.userId);
+	}
+
+	onCancelRun(context: Command.Context) {
+		return context.editOrReply('Exclusivo para desarrolladores');
+	}
+
+	async run(context: Command.Context) {
+		context.commandClient
+			.resetCommands()
+			.catch(() => {
+				return context.editOrReply('Error al recargar comandos');
+			})
+			.then(() => {
+				return context.editOrReply('Done');
+			});
+	}
+}
--- src/commands/prefix/owner/eval.ts
+++ src/commands/prefix/owner/eval.ts
@@ -1,11 +1,14 @@
 import { Command, CommandClient } from 'detritus-client';
 import { BaseCommand } from '../basecommand';
 import config from '../../../../config.json';
+import { inspect } from 'util'
 import { Markup } from 'detritus-client/lib/utils';
+import { exec } from 'child_process';
 export const COMMAND_NAME = 'eval';
 interface commandArgs {
 	eval: string;
 	async: boolean;
+	exec: boolean;
 }
 
 export default class EvalCommand extends BaseCommand {
@@ -13,7 +16,7 @@ export default class EvalCommand extends BaseCommand {
 		super(client, {
 			name: COMMAND_NAME,
 			aliases: ['ev'],
-			args: [{ name: 'async', type: Boolean }],
+			args: [{ name: 'async', type: Boolean }, { name: 'exec', type: Boolean }],
 			label: 'eval',
 			metadata: {
 				description: 'Ejecuta un código',
@@ -23,7 +26,7 @@ export default class EvalCommand extends BaseCommand {
 	}
 
 	onBeforeRun(context: Command.Context) {
-		return config.devsIds.includes(context.userId);
+		return !!context.user.isClientOwner
 	}
 
 	onCancelRun(context: Command.Context) {
@@ -32,8 +35,14 @@ export default class EvalCommand extends BaseCommand {
 
 	async run(context: Command.Context, args: commandArgs) {
 		if (!args.eval) return context.editOrReply('Escribe el codigo');
-		let message;
-		let type;
+		if(args.async && args.exec) return;
+		if(args.exec){
+			exec(args.eval, (error, stdout) => {
+				return context.editOrReply(Markup.codeblock(String(stdout || error), { language:'js'}))
+			})
+		} else {
+			let message: string;
+			let type: string;
 		try {
 			if (args.async) {
 				const AsyncCode = Object.getPrototypeOf(async function () {}).constructor;
@@ -44,11 +53,13 @@ export default class EvalCommand extends BaseCommand {
 			}
 			if (typeof message === 'object') {
 				message = JSON.stringify(message, null, 2);
-				let type = 'json';
+				type = 'json';
 			}
 		} catch (error: any) {
-			message = error ? error.stack || error.message : error;
+			message = inspect( error, { depth: 9 })
 		}
 		return context.editOrReply(Markup.codeblock(String(message), { language: `${type || 'js'}`}));
+	}
+		
 	}
 }
